29221273c72202ca9f8800f6ab383d1c10f0589d
Haakon Ludvig Langeland Ervik <haakonervik@hotmail.com>
2020-05-24 20:05:24 +0200

    test(feat): Make test methods for Flow.py & run_convergence_study.py
    * TestFlow runs the flow model for various simplified grids. A quick
    way to test various aspects of the model
    * TestRunModelForConvergenceStudy runs the flow model through the
    run_model_for_convergence_study framework on similar grids.
    ** Currently, we are not able to properly test convergence except for
    incompressible flow without fractures (due to grid size constraints)

--- src/mastersproject/GTS/isc_modelling/tests/test_flow.py
+++ src/mastersproject/GTS/isc_modelling/tests/test_flow.py
@@ -1,3 +1,4 @@
+from typing import Optional, List
 
 import porepy as pp
 import numpy as np
@@ -13,98 +14,159 @@ class TestFlow:
 
         Goal: No negative pressure cells
         """
-
-        _sz = 0.1
-        time_step = pp.HOUR
-        params = FlowParameters(
-            head="TestRunModelForConvergenceStudy/test_2_fracs_unit_domain",
-            time_step=time_step,
-            end_time=time_step * 4,
-            fluid=pp.UnitFluid(),
+        _run_flow_models_helper(
+            sz=0.1,
+            incompressible=False,
+            head="test_1_frac_unit_domain",
             shearzone_names=["f1"],
-            mesh_args={
-                "mesh_size_frac": _sz,
-                "mesh_size_min": _sz,
-                "mesh_size_bound": _sz * 4,
-            },
-            source_scalar_borehole_shearzone=None,
-            well_cells=nd_injection_cell_center,
-            injection_rate=1,
-            frac_permeability=1,
-            intact_permeability=1,
+            time_step=pp.HOUR,
         )
-        setup = FlowISC(params)
-        network = fracture_network_1_frac_unit_domain()
-        gb = network.mesh(
-            mesh_args=params.mesh_args,
-            file_name=str(params.folder_name / "gmsh_frac_file"),
-        )
-        setup.gb = gb
-        pp.run_time_dependent_model(setup, {})
-
-        assert setup.neg_ind.size == 0
 
     def test_2_fracs_unit_domain(self):
         """ Test that we a basic 2-fracture setup runs as
-        expected with easy parameters. Goal: No negative pressure cells
+        expected with easy parameters.
+
+        Goal: No negative pressure cells
         """
-
-        _sz = 0.1
-        time_step = pp.HOUR
-        params = FlowParameters(
-            head="TestRunModelForConvergenceStudy/test_2_fracs_unit_domain",
-            time_step=time_step,
-            end_time=time_step * 4,
-            fluid=pp.UnitFluid(),
+        _run_flow_models_helper(
+            sz=0.1,
+            incompressible=False,
+            head="test_2_fracs_unit_domain",
             shearzone_names=["f1", "f2"],
-            mesh_args={
-                "mesh_size_frac": _sz,
-                "mesh_size_min": _sz,
-                "mesh_size_bound": _sz * 4,
-            },
-            source_scalar_borehole_shearzone=None,
-            well_cells=nd_injection_cell_center,
-            injection_rate=1,
-            frac_permeability=1,
-            intact_permeability=1,
+            time_step=pp.HOUR,
         )
-        setup = FlowISC(params)
-        network = fracture_network_2_fracs_unit_domain()
-        gb = network.mesh(
-            mesh_args=params.mesh_args,
-            file_name=str(params.folder_name / "gmsh_frac_file"),
+
+    def test_2_fracs_unit_domain_incompressible_flow(self):
+        """ Test that we a basic 2-fracture setup runs as
+        expected with easy parameters and incompressible flow.
+
+        Goal: No negative pressure cells
+        """
+        # here; _sz=0.3 doesn't work. (=660 3d cells)
+        #       _sz=0.2 works (=1262 3d cells)
+        _run_flow_models_helper(
+            sz=0.3,
+            incompressible=True,
+            head="test_2_fracs_unit_domain_incompressible_flow",
+            shearzone_names=["f1", "f2"],
         )
-        setup.gb = gb
-        pp.run_time_dependent_model(setup, {})
 
-        assert setup.neg_ind.size == 0
+    def test_1_frac_unit_domain_incompressible_flow(self):
+        """ Test that we a basic 1-fracture setup runs as
+        expected with easy parameters and incompressible flow.
 
-def fracture_network_1_frac_unit_domain():
-    domain = {
+        Goal: No negative pressure cells
+        """
+        # here; _sz=0.4 doesn't work. (=~200 3d cells)
+        #       _sz=0.3 works (=344 3d cells)
+        _run_flow_models_helper(
+            sz=0.3,
+            incompressible=True,
+            head="test_1_frac_unit_domain_incompressible_flow",
+            shearzone_names=["f1"],
+        )
+
+    def test_0_frac_unit_domain_incompressible_flow(self):
+        """ Test that we a basic 0-fracture setup runs as
+        expected with easy parameters and incompressible flow.
+
+        Goal: No negative pressure cells
+        """
+        # here; _sz=0.6 works (=24 3d cells)
+        # It seems like 24 3d cells is close to minimum reachable.
+
+        _run_flow_models_helper(
+            sz=0.6,
+            incompressible=True,
+            head="test_0_frac_unit_domain_incompressible_flow",
+            shearzone_names=None,
+        )
+
+
+def _run_flow_models_helper(
+        sz: float,
+        incompressible: bool,
+        head: str,
+        shearzone_names: Optional[List[str]],
+        time_step: float = None,
+) -> None:
+    """ Helper method for the test_flow setups
+
+    sz is related to mesh_args
+    incompressible turns on or off compressibility (and time stepping)
+        - If compressible, you should also set time_step
+    head is the path head. Usually, method name
+    shearzone_names is a list of names given to fractures
+    n_frac is the number of fractures to be constructed
+    """
+
+    fluid = pp.UnitFluid(11)
+    if incompressible:
+        # Consider an incompressible problem
+        fluid.COMPRESSIBILITY = 0
+        time_step = 1
+        end_time = 1
+    else:
+        time_step = time_step
+        end_time = time_step * 4
+
+    params = FlowParameters(
+        head=f"TestFlow/{head}",
+        fluid=fluid,
+        time_step=time_step,
+        end_time=end_time,
+        shearzone_names=shearzone_names,
+        mesh_args={
+            "mesh_size_frac": sz,
+            "mesh_size_min": sz,
+            "mesh_size_bound": sz * 4,
+        },
+        source_scalar_borehole_shearzone=None,
+        well_cells=nd_injection_cell_center,
+        injection_rate=1,
+        frac_permeability=1,
+        intact_permeability=1,
+        bounding_box={
+            'xmin': 0, 'ymin': 0, 'zmin': 0,
+            'xmax': 1, 'ymax': 1, 'zmax': 1
+        },
+    )
+    setup = FlowISC(params)
+    network = network_n_fractures(params.n_frac)
+    gb = network.mesh(
+        mesh_args=params.mesh_args,
+        file_name=str(params.folder_name / "gmsh_frac_file"),
+    )
+    setup.gb = gb
+    pp.run_time_dependent_model(setup, {})
+
+    assert setup.neg_ind.size == 0
+
+
+def network_n_fractures(n_frac: int) -> pp.FractureNetwork3d:
+    """ Create a unit domain in 3d with n (pre-defined) fractures
+
+    """
+    assert 0 <= n_frac <= 2, "Only implemented between 0 and 2 fractures"
+
+    bounding_box = {
         'xmin': 0, 'ymin': 0, 'zmin': 0,
         'xmax': 1, 'ymax': 1, 'zmax': 1
     }
+    network = pp.FractureNetwork3d(None, bounding_box)
 
-    # Define fractures
-    frac_pts1 = np.array(
-        [[0.15, 0.15, 0.8, 0.8],
-         [0, 0.9, 0.9, 0],
-         [0, 0, 0.9, 0.9]])
-    frac1 = pp.Fracture(frac_pts1)
+    if n_frac >= 1:
+        frac_pts1 = np.array(
+            [[0.15, 0.15, 0.8, 0.8],
+             [0, 0.9, 0.9, 0],
+             [0, 0, 0.9, 0.9]])
+        network.add(pp.Fracture(frac_pts1))
 
-    # Create fracture network and mesh it
-    frac_network = pp.FractureNetwork3d([frac1], domain)
-    return frac_network
+    if n_frac >= 2:
+        frac_pts2 = np.array(
+            [[1, 1, 0.15, 0.15],
+             [0, 1, 1, 0],
+             [0.15, 0.15, 1, 1]])
+        network.add(pp.Fracture(frac_pts2))
 
-def fracture_network_2_fracs_unit_domain():
-    """ Add another fracture to the previous fracture network"""
-    frac_pts2 = np.array(
-        [[1, 1, 0.15, 0.15],
-         [0, 1, 1, 0],
-         [0.15, 0.15, 1, 1]])
-    frac2 = pp.Fracture(frac_pts2)
-
-    frac_network = fracture_network_1_frac_unit_domain()
-    frac_network.add(frac2)
-
-    return frac_network
+    return network
---
+++ src/mastersproject/refinement/tests/test_run_convergence_study.py
@@ -0,0 +1,137 @@
+from pathlib import Path
+from typing import List, Optional
+
+import numpy as np
+import porepy as pp
+from GTS import FlowISC
+from GTS.isc_modelling.parameter import FlowParameters, nd_injection_cell_center
+from refinement.run_convergence_study import run_model_for_convergence_study
+
+
+class TestRunModelForConvergenceStudy:
+    """ Test run_model_for_convergence_study on various domains and parameters
+    """
+
+    def test_0_frac_unit_domain(self):
+        """ No fractures, incompressible
+        """
+        _run_convergence_study_helper(
+            head="test_2_fracs_unit_domain",
+            shearzone_names=None,
+            sz=0.3,
+            n_refinements=3,
+            intact_permeability=0.1,
+        )
+
+    # IMPORTANT: We are not able to run proper tests on fractured grids as we cannot
+    #           run more than 1 refinement due to constraints in number of cells.
+
+    def test_2_fracs_unit_domain(self):
+        """ 2 fractures, incompressible
+
+        # Note: The current mesh_args gives no negative cells, but we are not able
+        # to solve a second refinement. So this test cannot be run to its full intent.
+        # See test_grid_convergence.py instead.
+        """
+        _run_convergence_study_helper(
+            head="test_2_fracs_unit_domain",
+            shearzone_names=["f1", "f2"],
+            sz=0.3,
+            n_refinements=2,
+            intact_permeability=0.1,
+        )
+
+    def test_1_frac_unit_domain(self):
+        """ 1 fracture, incompressible
+
+        # Note: The current mesh_args gives no negative cells, but we are not able
+        # to solve a second refinement. So this test cannot be run to its full intent.
+        # See test_grid_convergence.py instead.
+        """
+        _run_convergence_study_helper(
+            head="test_1_frac_unit_domain",
+            shearzone_names=["f1"],
+            sz=0.3,
+            n_refinements=2
+        )
+
+
+def _run_convergence_study_helper(
+        head: str,
+        shearzone_names: Optional[List[str]],
+        sz: float,
+        n_refinements: int,
+        frac_permeability: float = 1,
+        intact_permeability: float = 1,
+) -> None:
+    """ Helper method for run_convergence_study tests
+
+    head is the path head. Usually, method name
+    shearzone_names is a list of names given to fractures
+    sz is related to mesh_args
+    n_refinements is the number of refinements to run
+    frac_permeability, intact_permeability: Optionally set custom permeability
+    """
+
+    fluid = pp.UnitFluid(11)
+    fluid.COMPRESSIBILITY = 0  # Consider an incompressible problem
+
+    params = FlowParameters(
+        head=f"TestRunModelForConvergenceStudy/{head}",
+        fluid=fluid,
+        shearzone_names=shearzone_names,
+        mesh_args={
+            "mesh_size_frac": sz,
+            "mesh_size_min": sz,
+            "mesh_size_bound": sz * 4,
+        },
+        source_scalar_borehole_shearzone=None,
+        well_cells=nd_injection_cell_center,
+        injection_rate=1,
+        frac_permeability=frac_permeability,
+        intact_permeability=intact_permeability,
+    )
+
+    network = network_n_fractures(params.n_frac)
+
+    gb_list, errors = run_model_for_convergence_study(
+        model=FlowISC,
+        run_model_method=pp.run_time_dependent_model,
+        network=network,
+        params=params,
+        n_refinements=n_refinements,
+        newton_params=None,
+        variable=['p_exp'],
+        variable_dof=[1],
+    )
+
+    print(errors)
+
+
+def network_n_fractures(n_frac: int) -> pp.FractureNetwork3d:
+    """ Create a unit domain in 3d with n (pre-defined) fractures
+
+    """
+    assert 0 <= n_frac <= 2, "Only implemented between 0 and 2 fractures"
+
+    bounding_box = {
+        'xmin': 0, 'ymin': 0, 'zmin': 0,
+        'xmax': 1, 'ymax': 1, 'zmax': 1
+    }
+    network = pp.FractureNetwork3d(None, bounding_box)
+
+    if n_frac >= 1:
+        frac_pts1 = np.array(
+            [[0.15, 0.15, 0.8, 0.8],
+             [0, 0.9, 0.9, 0],
+             [0, 0, 0.9, 0.9]])
+        network.add(pp.Fracture(frac_pts1))
+
+    if n_frac >= 2:
+        frac_pts2 = np.array(
+            [[1, 1, 0.15, 0.15],
+             [0, 1, 1, 0],
+             [0.15, 0.15, 1, 1]])
+        network.add(pp.Fracture(frac_pts2))
+
+    return network
