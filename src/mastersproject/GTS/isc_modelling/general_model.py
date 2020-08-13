fix: verify aperture computation (#76)
* fix: Update pp.ITERATE reference
* feat: update default params in parameter.py
* fix: non-linear Newton update for discretizations in isc_model.py
* feat: for fracture intersections, compute the mechanical aperture as
the maximum of the adjacent apertures.
* refactor: replace iterators by lists for fracture intersections
-- since we usually only have 2 adjacent fractures per intersection,
-- we always compute lists for intermediate computations.
* test: Add test that mechanical aperture for fracture intersections
are computed correctly.
* feat(TODO): pressure-controlled injection
* style: black, isort, flake8

src/mastersproject/GTS/isc_modelling/general_model.py
@@ -1,7 +1,7 @@
 import abc
 import logging
 import time
-from typing import Optional, Dict, List
+from typing import Dict, List, Optional
 
 import numpy as np
 
@@ -9,6 +9,7 @@ import porepy as pp
 from GTS.isc_modelling.parameter import BaseParameters
 from mastersproject.util.logging_util import timer
 from porepy.models.abstract_model import AbstractModel
+from pypardiso import spsolve
 
 logger = logging.getLogger(__name__)
 
@@ -140,7 +141,6 @@ class CommonAbstractModel(AbstractModel):
     @timer(logger, level="INFO")
     def assemble_and_solve_linear_system(self, tol: float) -> np.ndarray:
         """ Assemble a solve the linear system"""
-        from pypardiso import spsolve
 
         A, b = self.assembler.assemble_matrix_rhs()  # noqa
 

src/mastersproject/GTS/isc_modelling/isc_model.py
@@ -5,11 +5,10 @@ from typing import Dict
 import numpy as np
 
 import porepy as pp
-from GTS.isc_modelling.ISCGrid import create_grid
 from GTS.isc_modelling.contact_mechanics_biot import ContactMechanicsBiotBase
+from GTS.isc_modelling.ISCGrid import create_grid
 from GTS.isc_modelling.parameter import BiotParameters
-
-from mastersproject.util.logging_util import trace, timer
+from mastersproject.util.logging_util import timer, trace
 
 logger = logging.getLogger(__name__)
 
@@ -17,6 +16,7 @@ logger = logging.getLogger(__name__)
 class ISCBiotContactMechanics(ContactMechanicsBiotBase):
     def __init__(self, params: BiotParameters):
         super().__init__(params)
+        self.params = params
 
         # -- GRAVITY OPTIONS
 
@@ -169,23 +169,18 @@ class ISCBiotContactMechanics(ContactMechanicsBiotBase):
         See also specific_volume.
         """
         a_init = self.compute_initial_aperture(g, scaled=scaled)
-        a_mech = self.mechanical_aperture(g, from_iterate=from_iterate)
-        if not scaled:
-            # Mechanical aperture is scaled by default, whereas initial aperture (above)
-            # is not. This "upscaling" ensures they are consistent.
-            a_mech *= self.params.length_scale / pp.METER
+        a_mech = self.mechanical_aperture(g, scaled=scaled, from_iterate=from_iterate)
 
-        # TODO: This is commented out to test effects of aperture variations in injection cell.
-        # # Find the cells that are not injection (or sink) cells
-        # injection_tags = g.tags["well_cells"]
-        # not_injection_cells = 1 - np.abs(injection_tags)
-
-        # Only add the mechanical contribution to non-injection cells:
-        a_total = a_init + a_mech  # * not_injection_cells
+        a_total = a_init + a_mech
         return a_total
 
     def compute_initial_aperture(self, g: pp.Grid, scaled) -> np.ndarray:
-        """ Fetch the initial aperture. See __init__ for details """
+        """ Fetch the initial aperture
+
+        For 3d-matrix: unitary (aperture isn't really defined in 3d)
+        For 2d-fracture: fetch from the parameter dictionary
+        For 1d-intersection: Get the max from the two adjacent fractures
+        """
         aperture = np.ones(g.num_cells)
         nd = self.Nd
         gb = self.gb
@@ -198,13 +193,9 @@ class ISCBiotContactMechanics(ContactMechanicsBiotBase):
             aperture *= self.params.initial_fracture_aperture[shearzone]
         elif g.dim == nd - 2:
             master_grids = gb.node_neighbors(g, only_higher=True)
-            shearzones = (gb.node_props(g_h, "name") for g_h in master_grids)
-            apertures = np.fromiter(
-                (self.params.initial_fracture_aperture[sz] for sz in shearzones),
-                dtype=float,
-                count=len(master_grids),
-            )
-            aperture *= np.mean(apertures)
+            shearzones = [gb.node_props(g_h, "name") for g_h in master_grids]
+            apertures = [self.params.initial_fracture_aperture[sz] for sz in shearzones]
+            aperture *= np.max(apertures)
         else:
             raise ValueError("Not implemented 1d intersection points")
 
@@ -213,13 +204,21 @@ class ISCBiotContactMechanics(ContactMechanicsBiotBase):
 
         return aperture
 
-    def mechanical_aperture(self, g: pp.Grid, from_iterate: bool) -> np.ndarray:
+    def mechanical_aperture(
+        self, g: pp.Grid, scaled: bool, from_iterate: bool
+    ) -> np.ndarray:
         """ Compute aperture contribution from mechanical opening of fractures
+
+        For 3d-matrix: zeros (aperture isn't really defined in 3d)
+        For 2d-fracture: compute from the local displacement jump
+        For 1d-intersection: Get the max from the two adjacent fractures
 
         Parameters
         ----------
         g : pp.Grid
             a grid
+        scaled : bool
+            whether to scale the displacement jump, which has units [m]
         from_iterate : bool
             whether to fetch displacement jump from from iterate or from pp.STATE.
 
@@ -253,25 +252,27 @@ class ISCBiotContactMechanics(ContactMechanicsBiotBase):
             normal_jump = np.abs(u_mortar_local[-1, :])
             # tangential_jump = np.linalg.norm(u_mortar_local[:-1, :], axis=0)
 
+            # Mechanical aperture is scaled by default. "Upscale" if necessary.
+            if not scaled:
+                normal_jump *= self.params.length_scale / pp.METER
+
             aperture_contribution = normal_jump
             return aperture_contribution
 
         # -- Computations --
 
-        # No contribution in 3d
+        # No contribution in 3d-matrix
         if g.dim == nd:
             return np.zeros(g.num_cells)
 
+        # For 2d & 1d, fetch the master grids
         master_grids = gb.node_neighbors(g, only_higher=True)
-        n_edges = len(master_grids)
         de = [gb.edge_props((g, e)) for e in master_grids]
-        initialized = np.alltrue(
-            np.fromiter((pp.STATE in d for d in de), dtype=bool, count=n_edges)
-        )
+        initialized = np.alltrue([pp.STATE in d for d in de])
         if not initialized:
             return np.zeros(g.num_cells)
 
-        # In fractures
+        # In 2d-fractures
         elif g.dim == nd - 1:
             data_edge = gb.edge_props((g, nd_grid))
             aperture = _aperture_from_edge(data_edge)
@@ -283,42 +284,39 @@ class ISCBiotContactMechanics(ContactMechanicsBiotBase):
         elif g.dim == nd - 2:
             # (g is the slave grid.)
             # Fetch edges of g that points to a higher-dimensional grid
-            intx_edges = ((g, g_h) for g_h in master_grids)
-            nd_edges = ((g_h, nd_grid) for g_h in master_grids)
-            cell_faces = (g_h.cell_faces for g_h in master_grids)
+            intx_edges = [(g, g_h) for g_h in master_grids]
+            frac_edges = [(g_h, nd_grid) for g_h in master_grids]
+            frac_cell_faces = [g_h.cell_faces for g_h in master_grids]
 
-            data_edges = (gb.edge_props(edge) for edge in nd_edges)
-            master_apertures = (
+            # get apertures on the adjacent fractures
+            data_edges = [gb.edge_props(edge) for edge in frac_edges]
+            frac_apertures = [
                 _aperture_from_edge(data_edge) for data_edge in data_edges
-            )
+            ]
 
-            # Map parent apertures to faces
-            master_face_apertures = (
+            # Map fracture apertures to internal faces ..
+            frac_face_apertures = [
                 np.abs(cell_face) * parent_aperture
-                for cell_face, parent_aperture in zip(cell_faces, master_apertures)
+                for cell_face, parent_aperture in zip(frac_cell_faces, frac_apertures)
+            ]
+
+            # .. then project face apertures to the interfaces and take maximum
+            # Note: for matching grids, avg and int mortar projections are identical.
+            mortar_grids = [gb.edge_props(edge)["mortar_grid"] for edge in intx_edges]
+            mortar_apertures = [
+                mg.master_to_mortar_int() * ap
+                for mg, ap in zip(mortar_grids, frac_face_apertures)
+            ]
+            # The reshape and max operations implicitly project the aperture to
+            # the intersection grid (assuming a conforming mesh).
+            intx_max_apertures = np.max(
+                np.vstack(
+                    [mortar_ap.reshape((2, -1)) for mortar_ap in mortar_apertures]
+                ),
+                axis=0,
             )
 
-            # .. and project face apertures to slave grid
-            mortar_grids = (gb.edge_props(edge)["mortar_grid"] for edge in intx_edges)
-
-            # Use _int() here to sum apertures, then we average at the end.
-            master_to_slave_apertures = (
-                mg.mortar_to_slave_int()
-                * mg.master_to_mortar_int()
-                * master_face_aperture
-                for mg, master_face_aperture in zip(mortar_grids, master_face_apertures)
-            )
-
-            # expand the iterator
-            apertures = np.zeros((n_edges, g.num_cells))
-            for i, ap in enumerate(master_to_slave_apertures):
-                apertures[i, :] = ap
-
-            # average the apertures from master to determine the slave aperture
-            avg_aperture = np.mean(
-                apertures, axis=0
-            )  # / 2  <-- divide by 2 for each mortar side???
-            return avg_aperture
+            return intx_max_apertures
 
         else:
             raise ValueError("Not implemented 1d intersection points")
@@ -360,32 +358,14 @@ class ISCBiotContactMechanics(ContactMechanicsBiotBase):
 
     def source_scalar_pressure(self, pressure):
         """ Pressure-controlled injection"""
+        # TODO: Implement implicit pressure-controlled injection
         for g, d in self.gb:
             wells = g.tags["well_cells"]
             if np.sum(np.abs(wells)) > 0:
-                dof_ind = self.assembler.dof_ind(g, self.scalar_variable)
-                loc = np.where(wells != 0)[0]
-                glob_ind = dof_ind[loc]
-                A, b = self.assembler.assemble_matrix_rhs(matrix_format="csr")
-                self.csr_zero_rows(A, glob_ind)
-                A[glob_ind, glob_ind] = 1
-                b[glob_ind] = pressure
-
-    def csr_zero_rows(self, csr, rows_to_zero):
-        """ Efficient way to set csr sparse matrix row to zero.
-        https://stackoverflow.com/questions/19784868/
-        what-is-most-efficient-way-of-setting-row-to-zeros-for-a-sparse-scipy-matrix/
-        19800305#19800305"""
-        rows, cols = csr.shape
-        mask = np.ones((rows,), dtype=np.bool)
-        mask[rows_to_zero] = False
-        nnz_per_row = np.diff(csr.indptr)
-
-        mask = np.repeat(mask, nnz_per_row)
-        nnz_per_row[rows_to_zero] = 0
-        csr.data = csr.data[mask]
-        csr.indices = csr.indices[mask]
-        csr.indptr[1:] = np.cumsum(nnz_per_row)
+                pass
+                # dof_ind = self.assembler.dof_ind(g, self.scalar_variable)
+                # loc = np.where(wells != 0)[0]
+                # glob_ind = dof_ind[loc]
 
     # --- Simulation and solvers ---
 
@@ -401,7 +381,9 @@ class ISCBiotContactMechanics(ContactMechanicsBiotBase):
     def before_newton_iteration(self) -> None:
         # Re-discretize the nonlinear term
         super().before_newton_iteration()
-        self.assembler.discretize(term_filter=["!grad_p", "!div_u", "!stabilization"])
+        self.assembler.discretize(
+            term_filter=["!grad_p", "!div_u", "!stabilization", "!mpsa"]
+        )
         # for g, _ in self.gb:
         #     if g.dim < self.Nd:
         #         self.assembler.discretize(grid=g)

src/mastersproject/GTS/isc_modelling/mechanics.py
@@ -2,12 +2,12 @@ import logging
 from typing import Dict, Tuple
 
 import numpy as np
-import pendulum
 
 import GTS as gts
+import pendulum
 import porepy as pp
-from GTS.isc_modelling.ISCGrid import create_grid
 from GTS.isc_modelling.general_model import CommonAbstractModel
+from GTS.isc_modelling.ISCGrid import create_grid
 from GTS.isc_modelling.parameter import BaseParameters, GrimselGranodiorite
 from mastersproject.util.logging_util import timer, trace
 from porepy.models.contact_mechanics_model import ContactMechanics
@@ -252,7 +252,7 @@ class Mechanics(CommonAbstractModel):
         The contact pressure is set to zero in the tangential direction,
         and -1 (that is, in contact) in the normal direction.
 
-        We initialize "previous_iterate" for the contact traction and
+        We initialize pp.ITERATE for the contact traction and
         mortar displacement since they are to be updated every Newton
         iteration.
 
@@ -261,7 +261,7 @@ class Mechanics(CommonAbstractModel):
         var_m = self.displacement_variable
         var_contact = self.contact_traction_variable
         var_mortar = self.mortar_displacement_variable
-        state = pp.STATE
+        state, iterate = pp.STATE, pp.ITERATE
 
         for g, d in gb:
             add_nonpresent_dictionary(d, state)
@@ -278,10 +278,7 @@ class Mechanics(CommonAbstractModel):
                     (np.zeros((g.dim, g.num_cells)), -1 * np.ones(g.num_cells))
                 ).ravel(order="F")
                 d[state].update(
-                    {
-                        "previous_iterate": {var_contact: traction},
-                        var_contact: traction,
-                    }
+                    {iterate: {var_contact: traction}, var_contact: traction,}
                 )
 
         for e, d in self.gb.edges():
@@ -291,10 +288,7 @@ class Mechanics(CommonAbstractModel):
             if mg.dim == self.Nd - 1:
                 size = mg.num_cells * self.Nd
                 d[state].update(
-                    {
-                        var_mortar: np.zeros(size),
-                        "previous_iterate": {var_mortar: np.zeros(size)},
-                    }
+                    {var_mortar: np.zeros(size), iterate: {var_mortar: np.zeros(size)},}
                 )
 
     # --- Simulation and solvers ---
@@ -501,7 +495,7 @@ class Mechanics(CommonAbstractModel):
 
         Extract parts of the solution for current iterate.
 
-        The iterate solutions in d[pp.STATE]["previous_iterate"]
+        The iterate solutions in d[pp.STATE][pp.ITERATE]
         are updated for
             - mortar displacements
             - contact traction
@@ -532,14 +526,14 @@ class Mechanics(CommonAbstractModel):
                     if name == var_mortar:
                         mortar_u = (solution_vector[dof[bi] : dof[bi + 1]]).copy()
                         data = self.gb.edge_props(g)
-                        data[pp.STATE]["previous_iterate"][var_mortar] = mortar_u
+                        data[pp.STATE][pp.ITERATE][var_mortar] = mortar_u
                 else:
                     # g is a node/grid (not edge)
                     # For the fractures, update the contact force
                     if (g.dim < self.Nd) and (name == var_contact):
                         contact = (solution_vector[dof[bi] : dof[bi + 1]]).copy()
                         data = self.gb.node_props(g)
-                        data[pp.STATE]["previous_iterate"][var_contact] = contact
+                        data[pp.STATE][pp.ITERATE][var_contact] = contact
 
     def _is_nonlinear_problem(self) -> bool:
         """
@@ -564,7 +558,7 @@ class Mechanics(CommonAbstractModel):
 
         """
         # TODO: Currently 'reconstruct_stress' does not work if 'previous_iterate = True'
-        #  since the displacement variable on Nd-grid is not stored in "previous_iterate".
+        #  since the displacement variable on Nd-grid is not stored in pp.ITERATE.
         if previous_iterate is True:
             raise ValueError("Not yet implemented.")
         g = self._nd_grid()
@@ -576,7 +570,7 @@ class Mechanics(CommonAbstractModel):
         mpsa = pp.Mpsa(self.mechanics_parameter_key)
 
         if previous_iterate:
-            u = d[pp.STATE]["previous_iterate"][var_m]
+            u = d[pp.STATE][pp.ITERATE][var_m]
         else:
             u = d[pp.STATE][var_m]
 
@@ -626,7 +620,7 @@ class Mechanics(CommonAbstractModel):
 
         mg: pp.MortarGrid = data_edge["mortar_grid"]
         if from_iterate:
-            mortar_u = data_edge[pp.STATE]["previous_iterate"][var_mortar]
+            mortar_u = data_edge[pp.STATE][pp.ITERATE][var_mortar]
         else:
             mortar_u = data_edge[pp.STATE][var_mortar]
 
@@ -1266,7 +1260,7 @@ class ContactMechanicsISC(ContactMechanics):
         """
         Extract parts of the solution for current iterate.
 
-        The iterate solutions in d[pp.STATE]["previous_iterate"] are updated for the
+        The iterate solutions in d[pp.STATE][pp.ITERATE] are updated for the
         mortar displacements and contact traction are updated.
         Method is a tailored copy from assembler.distribute_variable.
 

src/mastersproject/GTS/test/test_isc_modelling/test_contact_mechanics_biot.py
@@ -1,11 +1,12 @@
-import pytest
 from pathlib import Path
+
 import numpy as np
+import pytest
 
 import porepy as pp
 from GTS.isc_modelling.contact_mechanics_biot import ContactMechanicsBiotBase
-from GTS.isc_modelling.mechanics import Mechanics
 from GTS.isc_modelling.flow import Flow
+from GTS.isc_modelling.mechanics import Mechanics
 from GTS.isc_modelling.parameter import BaseParameters
 from GTS.test.standard_grids import two_intersecting_blocking_fractures
 from porepy.utils.derived_discretizations import implicit_euler
@@ -181,7 +182,7 @@ class TestContactMechanicsBiotBase:
 
             elif g.dim == nd - 1:
                 traction = d[pp.STATE][var_contact]
-                prev_traction = d[pp.STATE]["previous_iterate"][var_contact]
+                prev_traction = d[pp.STATE][pp.ITERATE][var_contact]
                 init_traction = np.vstack(
                     (np.zeros((g.dim, g.num_cells)), -1 * np.ones(g.num_cells))
                 ).ravel(order="F")
@@ -197,7 +198,7 @@ class TestContactMechanicsBiotBase:
             # mechanics
             if mg.dim == nd - 1:
                 mortar_m = d[pp.STATE][var_mortar_m]
-                prev_mortar_m = d[pp.STATE]["previous_iterate"][var_mortar_m]
+                prev_mortar_m = d[pp.STATE][pp.ITERATE][var_mortar_m]
                 assert np.allclose(mortar_m, np.zeros(mg.num_cells * nd))
                 assert np.allclose(prev_mortar_m, np.zeros(mg.num_cells * nd))
 