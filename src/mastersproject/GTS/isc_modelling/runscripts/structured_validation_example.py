ed3919d67a55e16fe3b817ec167d0a61c4e4b5d7
Haakon Ludvig Langeland Ervik <45243236+haakon-e@users.noreply.github.com>
2020-08-26 17:04:32 +0200

    test: test runscripts

---
+++ src/mastersproject/GTS/isc_modelling/runscripts/structured_validation_example.py
@@ -0,0 +1,21 @@
+import numpy as np
+import porepy as pp
+
+
+def structured_grid_2_frac_vertical(length_scale: float):
+    nx = np.array([20, 20, 20])
+    physdims = np.array([300, 300, 300])
+
+    # fmt: off
+    frac_pts1 = np.array(
+        [[0, 300, 300, 0],
+         [150, 150, 150, 150],
+         [0, 0, 150, 150]])
+
+    frac_pts2 = np.array(
+        [[0, 300, 300, 0],
+         [150, 150, 150, 150],
+         [0, 0, 150, 150]])
+    # fmt: on
+    gb = pp.meshing.cart_grid([frac_pts1, frac_pts2], nx=nx, physdims=physdims / length_scale,)
+    return gb
