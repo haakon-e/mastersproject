fix: isc simulation scaling (#83)
* feat: enable k-inv scaling of RobinCoupling scalar term
kinv scaling improves condition number by ~5 orders of magnitude
* refactor: optimal scaling methods for isc_simulation.py runscript
* fix: remove double contribution of dilation
* sim(isc): update timestep, max newton iter, path, and default dilation angle

src/mastersproject/GTS/isc_modelling/isc_model.py
@@ -275,12 +275,13 @@ class ISCBiotContactMechanics(ContactMechanicsBiotBase):
             )
             # Jump distances in each cell
             normal_jump = np.abs(u_mortar_local[-1, :])
-
+            
+            # NOTE: The normal jump already includes the dilation!
             # Slip induced dilation
-            tangential_jump = np.linalg.norm(u_mortar_local[:-1, :], axis=0)
-            dilation = np.tan(self.params.dilation_angle) * tangential_jump
+            #tangential_jump = np.linalg.norm(u_mortar_local[:-1, :], axis=0)
+            #dilation = np.tan(self.params.dilation_angle) * tangential_jump
 
-            aperture_contribution = normal_jump + dilation
+            aperture_contribution = normal_jump #+ dilation
 
             # Mechanical aperture is scaled by default. "Upscale" if necessary.
             if not scaled:
@@ -524,6 +525,23 @@ class ISCBiotContactMechanics(ContactMechanicsBiotBase):
 
             # Set intersection transient source term
             scalar_params["source"] += self.intersection_volume_iterate(g)
+
+    # --- Other flow related methods ---
+
+    def assign_scalar_discretizations(self) -> None:
+        """ Assign k inverse scaling to the coupling discretization
+
+        From IvaR:
+        For long time steps, scaling the diffusive interface fluxes in the non-default
+        way turns out to actually be beneficial for the condition number.
+        """
+        super().assign_scalar_discretizations()
+
+        for e, d in self.gb.edges():
+            d[pp.COUPLING_DISCRETIZATION][self.scalar_coupling_term][e][
+                1
+            ].kinv_scaling = True
+
 
     # --- MECHANICS ---
 

src/mastersproject/GTS/isc_modelling/runscripts/isc_simulation.py
@@ -6,12 +6,12 @@ import porepy as pp
 import numpy as np
 
 from GTS import ISCBiotContactMechanics, BiotParameters
-from GTS.isc_modelling.optimal_scaling import best_cond_numb
+from GTS.isc_modelling.optimal_scaling import best_cond_numb, condition_number_porepy
 from GTS.isc_modelling.parameter import shearzone_injection_cell
 from GTS.time_machine import NewtonParameters, TimeMachinePhasesConstantDt
 from GTS.time_protocols import TimeStepProtocol, InjectionRateProtocol
 
-logging.basicConfig(level=logging.INFO)
+#logging.basicConfig(level=logging.INFO)
 
 
 def prepare_setup(
@@ -20,9 +20,11 @@ def prepare_setup(
     """ Validation on the ISC grid"""
     injection_protocol, time_params = isc_dt_and_injection_protocol()
 
-    newton_params = NewtonParameters(convergence_tol=1e-6, max_iterations=30,)
-    sz = 5
-    path = Path(__file__).parent / "isc_simulation/scaling-delete-me"
+    newton_params = NewtonParameters(convergence_tol=1e-6, max_iterations=200,)
+    sz = 10
+    # path = Path(__file__).parent / "isc_simulation/scaling-delete-me"
+    root = Path.home()
+    path = root / "mastersproject-data/results/200828/coarse-tests/t1-gravity"
     biot_params = BiotParameters(
         # BaseParameters
         length_scale=length_scale,
@@ -34,43 +36,55 @@ def prepare_setup(
         end_time=time_params.end_time,
         gravity=True,
         # GeometryParameters
-        shearzone_names=["S1_2", "S3_1"],
+        shearzone_names=["S1_2"],  #, "S1_1"],
         mesh_args={
             "mesh_size_frac": sz,
-            "mesh_size_min": 0.2 * sz,
+            "mesh_size_min": 0.1 * sz,
             "mesh_size_bound": 3 * sz,
         },
         # MechanicsParameters
-        dilation_angle=np.radians(10),
+        dilation_angle=np.radians(3),
         newton_options=newton_params.dict(),
         # FlowParameters
         well_cells=shearzone_injection_cell,
         injection_protocol=injection_protocol,
-        frac_transmissivity=[1e-9, 3.7e-7],
+        frac_transmissivity=1e-9,  #[1e-9, 3.7e-7],
     )
     setup = ISCBiotContactMechanics(biot_params)
+    print(path)
     return setup, newton_params, time_params
 
 
 def validation():
     setup, newton_params, time_params = prepare_setup(
-        length_scale=12.8, scalar_scale=10 * pp.GIGA,
+        length_scale=0.01,
+        scalar_scale=1e6,
     )
     time_machine = TimeMachinePhasesConstantDt(setup, newton_params, time_params)
 
     time_machine.run_simulation()
     return time_machine
 
+# --- CONDITION NUMBER ---
+
+def cond_num_isc(ls, log_ss):
+    values = np.array([ls, log_ss])
+    A = assemble_matrix(values)
+    cond = condition_number_porepy(A)
+    print(f"cond pp: {cond:.2e}")
+
+
+def assemble_matrix(values):
+    ls, log_ss = values
+    ss = np.float_power(10, log_ss)
+    biot_params, newton, time = prepare_params(length_scale=ls, scalar_scale=ss,)
+    setup = ISCBiotContactMechanics(biot_params)
+    setup.prepare_simulation()
+    A, b = setup.assembler.assemble_matrix_rhs()
+    return A
+
 
 def optimal_scaling():
-    def assemble_matrix(values):
-        ls, log_ss = values
-        ss = np.float_power(10, log_ss)
-        setup, newton, time = prepare_setup(length_scale=ls, scalar_scale=ss,)
-        setup.prepare_simulation()
-        A, b = setup.assembler.assemble_matrix_rhs()
-        return A
-
     initial_guess = np.array([0.05, 6])
     results = best_cond_numb(assemble_matrix, initial_guess)
     return results
@@ -92,19 +106,33 @@ def isc_dt_and_injection_protocol():
     _1min = pp.MINUTE
     _10min = 10 * _1min
     phase_limits = [
-        -1e2 * pp.YEAR,
+        -10e3 * pp.YEAR,
         0,
         _10min,
-        2 * _10min,
-        3 * _10min,
-        4 * _10min,
-        7 * _10min,
+    #    2 * _10min,
+    #    3 * _10min,
+    #    4 * _10min,
+    #    7 * _10min,
     ]
-    rates = [0, 10, 15, 20, 25, 0]
+    rates = [
+        0, 
+        10, 
+    #    15, 
+    #    20, 
+    #    25, 
+    #    0,
+    ]
     rates = [r / 60 for r in rates]  # Convert to litres / second
     injection_protocol = InjectionRateProtocol.create_protocol(phase_limits, rates)
 
-    time_steps = [1e2 * pp.YEAR / 2, _1min, _1min, _1min, _1min, 3 * _1min]
+    time_steps = [
+        5e3 * pp.YEAR, 
+        1*_1min, 
+    #    _1min, 
+    #    _1min, 
+    #    _1min, 
+    #    3 * _1min,
+    ]
     time_step_protocol = TimeStepProtocol.create_protocol(phase_limits, time_steps)
 
     return injection_protocol, time_step_protocol